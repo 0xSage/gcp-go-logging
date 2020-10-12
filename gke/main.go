d1ab8da4addfdb3329d3be0d8b4a43f2ad954e78
nicoleczhu <nicolezhu@google.com>
2020-10-12 09:43:57 -0700

    add gke and run

---
+++ gke/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+        "os"
+        "context"
+        "cloud.google.com/go/logging"
+)
+
+//                assembles http response    obj/data structure of incoming request
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w,r)
+    return
+  }
+  fmt.Fprint(w, "Hello World!")
+}
+
+// To deploy:
+// gcloud builds submit --tag gcr.io/go-logging/run
+// gcloud run deploy --image gcr.io/go-logging/run --platform managed
+func main() {
+  ctx := context.Background()
+  projectID := "go-logging"
+
+  // Create a logging client
+  client, err := logging.NewClient(ctx, projectID)
+  if err != nil {
+    log.Fatalf("Failed to create logging client: %V", err)
+  }
+
+  defer client.Close()
+  // Sets name of log to write to
+  logger := client.Logger("cloudrun-log")
+
+  log.Print("hello world!")
+  logger.Log(logging.Entry{Payload: "Hello world!"})
+  logger.StandardLogger(logging.Info).Println("Hello World!")
+
+  // Handle routes & ports
+  http.HandleFunc("/", indexHandler)
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  logger.Log(logging.Entry{Payload: "Listening on port: 8080"})
+
+  // Start server
+  if err := http.ListenAndServe(":"+port, nil); err != nil {  
+    log.Fatal(err)
+  }
+}
---
+++ run/go.mod
@@ -0,0 +1,5 @@
+module github.com/nicoleczhu/gcp-go-logging/run/main
+
+go 1.15
+
+require cloud.google.com/go/logging v1.1.0
